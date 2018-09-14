931fedd43560c82b0655cad9dfce700823927299
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-09-14 17:39:10 +0300

    update: addTodoItem component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { DoneTodoItemsComponent } from './components/done-todo-items/done-todo-items.component';
+import { AddTodoItemComponent } from './components/add-todo-item/add-todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoItemComponent,
-    DoneTodoItemsComponent
+    DoneTodoItemsComponent,
+    AddTodoItemComponent
   ],
   imports: [
     BrowserModule
